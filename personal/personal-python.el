93927845f02c6d5d2285e3b1a031710d3251d27a
Jean Dupin <jean.dupin@111cap.com>
2025-01-22 10:07:46 +0100

    Clean up python personal config

--- personal/personal-python.el
+++ personal/personal-python.el
@@ -8,12 +8,8 @@
 
 ;;; Code:
 
-;; Set python3 as default python interpreter
-(setq python-shell-interpreter "~/.pyenv/versions/3.9.12/bin/python3")
-
 ;; Set IPython as default python interpreter
 (setq python-shell-interpreter "ipython")
-      ;; python-shell-interpreter-args "--simple-prompt -c exec('__import__(\\'readline\\')') -i")
 
 (provide 'personal-python)
 ;;; personal-python.el ends here
